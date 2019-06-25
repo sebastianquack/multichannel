daa49b10975cf415844bdd10f3d021d8b7b050bb
Sebastian Quack <Sebastian Quack>
2019-06-25 19:06:13 +0200

    react production build

---
+++ api/admin/precache-manifest.7570151976d0b19ec9c87b3f03178ec7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec4abe746de0966f7c45f99012bcf080",
+    "url": "/index.html"
+  },
+  {
+    "revision": "34c21af226cae881ad84",
+    "url": "/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "45f271f38fd4a4b73b67",
+    "url": "/static/js/2.a25a9539.chunk.js"
+  },
+  {
+    "revision": "34c21af226cae881ad84",
+    "url": "/static/js/main.18da581b.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
--- api/server.js
+++ api/server.js
@@ -2,6 +2,9 @@ let Hapi = require('hapi')
 let mongoose = require('mongoose')
 let RestHapi = require('rest-hapi')
 
+const Path = require('path');
+const Inert = require('@hapi/inert');
+
 if(process.env.NODE_ENV != "production") {
   require('dotenv-safe').config()  
 }
@@ -9,12 +12,43 @@ if(process.env.NODE_ENV != "production") {
 async function api() {
   try {
     let server = Hapi.Server({ 
-      port: process.env.PORT
+      port: process.env.PORT,
+      routes: {
+        files: {
+          relativeTo: Path.join(__dirname, 'admin')
+        }
+      }
     })
+
+    await server.register(Inert);
+
+    server.route({
+        method: 'GET',
+        path: '/admin/{param*}',
+        handler: {
+            directory: {
+                path: './index.html',
+                redirectToSlash: true
+            }
+        }
+    });
+
+    server.route({
+        method: 'GET',
+        path: '/static/{param*}',
+        handler: {
+            directory: {
+                path: './static',
+                redirectToSlash: true
+            }
+        }
+    });
+
 
     let config = {
       appTitle: "testresthapi",
       version: "1.0.0",
+      apiPath: 'custom_endpoints',
       mongo: {
         URI: process.env.MONGO_URL
       }
